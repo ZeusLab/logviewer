7d2a21d5eb8b60cf7b32edfe3416cc5f3c56d900
Loc Ngo <locngo@fortna.com>
2020-05-15 11:20:16 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import {
     Row,
     Col,
     Card,
+    Dropdown,
+    DropdownButton,
 } from 'react-bootstrap';
 import './App.css';
 import chainedFunction from 'chained-function';
+import BootstrapTable from 'react-bootstrap-table-next';
 import "./index.styl";
 
 class MenuItem extends PureComponent {
@@ -125,46 +128,24 @@ class LeftMenu extends React.Component {
     }
 }
 
-class LogDisplay extends React.Component {
+class LogDisplayContent extends React.Component {
 
     constructor(props) {
         super(props);
-        const {
-            application,
-            onLoadCompleted,
-        } = this.props;
-        this.onLoadCompleted = onLoadCompleted;
         this.state = {
-            application: application,
+            data: [],
+            columns: [{
+                dataField: 'fluentd_time',
+                text: 'Timestamp',
+                headerStyle: {
+                    width: '20%',
+                }
+            }, {
+                dataField: 'message',
+                text: 'Message',
+            }],
         }
     }
-
-    changeApplication = (application) => {
-        this.setState({
-            application: application,
-        })
-        this.fetchingHistories(application)
-    };
-
-    fetchingHistories = (tag) => {
-        fetch("/histories?tag=" + tag)
-            .then(responseMsg => {
-                return responseMsg.json();
-            })
-            .then(responseJson => {
-                if (responseJson.code === 200) {
-                    this.fetchingLog(tag, responseJson.data[0], 0)
-                } else {
-                    /**
-                     * alert log
-                     */
-                }
-            })
-            .catch(error => {
-            })
-            .finally(() => {
-            })
-    };
 
     fetchingLog = (tag, date, id) => {
         console.log("fetching log");
@@ -176,7 +157,112 @@ class LogDisplay extends React.Component {
                 return responseMsg.json();
             })
             .then(responseJson => {
+                console.log(responseJson);
                 if (responseJson.code !== 200) {
+                    this.setState({
+                        data: [],
+                    });
+                } else {
+                    this.setState({
+                        data: responseJson.data,
+                    });
+
+                }
+            })
+            .catch(error => {
+            })
+            .finally(() => {
+                this.props.onLoadCompleted();
+            })
+    };
+
+    componentDidMount() {
+        this.reload();
+    }
+
+    reload = () => {
+        const {
+            application,
+            date,
+        } = this.props;
+        if (date === undefined || date === "") {
+            return;
+        }
+        this.fetchingLog(application, date, 0);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('did update application ', this.props, prevProps);
+        if (this.props.date !== prevProps.date) {
+            this.reload();
+        }
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('get update application');
+        if (props.application !== state.application) {
+            return {
+                data: [],
+                application: props.application,
+            };
+        }
+
+        if (props.date !== state.date) {
+            return {
+                data: [],
+                date: props.date,
+            }
+        }
+        return null;
+    }
+
+
+    render() {
+        const data = this.state.data;
+        console.log(data);
+        const columns = this.state.columns;
+        const options = {
+            container: true, // use this scrollable element as a container
+            initialState: {
+                firstItemIndex: 0, // show first ten items
+                lastItemIndex: 9,  // during initial render
+            },
+        };
+        return (
+            <div className="message-table">
+                {/*<BootstrapTable*/}
+                {/*    keyField="id"*/}
+                {/*    data={data}*/}
+                {/*    columns={columns}*/}
+                {/*    bordered={false}*/}
+                {/*    headerClasses="message-table-header"*/}
+                {/*/>*/}
+            </div>
+        )
+    }
+}
+
+class LogDisplayHeader extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentDate: undefined,
+            histories: []
+        };
+    }
+
+    fetchingHistories = (tag) => {
+        fetch("/histories?tag=" + tag)
+            .then(responseMsg => {
+                return responseMsg.json();
+            })
+            .then(responseJson => {
+                console.log(responseJson);
+                if (responseJson.code === 200) {
+                    this.setState({
+                        histories: responseJson.data,
+                    });
                 } else {
                     /**
                      * alert log
@@ -186,25 +272,112 @@ class LogDisplay extends React.Component {
             .catch(error => {
             })
             .finally(() => {
-                this.onLoadCompleted();
             })
     };
 
     componentDidMount() {
-        /**
-         * do nothing now
-         */
+        const application = this.props.application;
+        this.fetchingHistories(application);
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.application !== prevProps.application) {
+            this.fetchingHistories(this.props.application);
+        }
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        if (props.application !== state.application) {
+            return {
+                application: props.application,
+            };
+        }
+        return null;
+    }
+
+    onDateSelected = (eventKey, eventObject) => {
+        this.props.onDateSelected(eventKey, eventObject);
+        this.setState({
+            currentDate: eventKey,
+        });
+    };
+
     render() {
-        let app = this.state.application;
+        const histories = this.state.histories;
+        const title = this.state.currentDate === undefined ? "Select date" : this.state.currentDate;
+        return (
+            <div className="header">
+                <div className="date-area">
+                    <span className="float-left">Date </span>
+                    <DropdownButton id="dropdown-basic-button"
+                                    className="float-left"
+                                    title={title}
+                                    onSelect={this.onDateSelected}>
+                        {histories.map((value, index) => {
+                            return (
+                                <Dropdown.Item key={value.key}
+                                               eventKey={value.key}>
+                                    {value.value}
+                                </Dropdown.Item>
+                            );
+                        })}
+                    </DropdownButton>
+                </div>
+
+                <div className="control-area">
+                </div>
+            </div>
+        );
+    }
+}
+
+class LogDisplay extends React.Component {
+
+    state = {};
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: undefined,
+        };
+    }
+
+    onDateSelected = (eventKey, eventObject) => {
+        console.log(eventKey, eventObject);
+        this.setState({
+            date: eventKey,
+        });
+    };
+
+    static getDerivedStateFromProps(props, state) {
+        if (props.application !== state.application) {
+            return {
+                application: props.application,
+            };
+        }
+        return null;
+    }
+
+    onLoadMessageCompleted = () => {
+        this.props.onLoadCompleted();
+    };
+
+    render() {
+        let app = this.props.application;
         if (app === undefined || app === "") {
             return (<React.Fragment></React.Fragment>);
         }
         return (
             <Card className="full-screen">
-                <Card.Body className="empty-log">
-                    {this.state.application}
+                <Card.Body>
+                    <LogDisplayHeader
+                        application={app}
+                        onDateSelected={this.onDateSelected}/>
+                    <LogDisplayContent
+                        application={app}
+                        onLoadCompleted={this.onLoadMessageCompleted}
+                        date={this.state.date}
+                    />
                 </Card.Body>
             </Card>
         )
@@ -215,14 +388,16 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.currentApp = React.createRef();
+        this.state = {
+            application: undefined
+        }
     }
 
     onAppSelected = (application) => {
         this.setState({
             isActive: true,
-        })
-        this.currentApp.current.changeApplication(application);
+            application: application,
+        });
     };
 
     onLoadCompleted = () => {
@@ -243,7 +418,7 @@ class App extends React.Component {
                         <Col mx={10} lg={10} xl={10}>
                             <div className="log-display">
                                 <LogDisplay
-                                    ref={this.currentApp}
+                                    application={this.state.application}
                                     onLoadCompleted={this.onLoadCompleted}/>
                             </div>
                         </Col>
