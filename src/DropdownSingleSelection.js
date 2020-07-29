2660c7368269a3657b2abe2abafb12753f68f6e9
Loc Ngo <xuanloc0511@gmail.com>
2020-07-29 14:57:10 +0700

    Limit max date are selected

--- src/DropdownSingleSelection.js
+++ src/DropdownSingleSelection.js
@@ -7,13 +7,16 @@ function DropdownSingleSelection(props) {
 		defaultValue,
 		value,
 		placeHolder,
+		disabled,
 		onChange
 	} = props;
 	return (
 		<Dropdown
 			placeholder={placeHolder}
 			fluid
+			disabled={disabled}
 			selection
+			readOnly={true}
 			defaultValue={defaultValue}
 			value={value}
 			className="dropdown-single"
--- src/QueryExplanation.js
+++ src/QueryExplanation.js
@@ -3,23 +3,27 @@ import {LogLevelStr, TimeOptionString} from "./LogDisplayHeader";
 import moment from "moment";
 
 function QueryExplanation(props) {
-	const logLevel = LogLevelStr(props.logLevel);
-	const timeOption = TimeOptionString(props.timeOption);
-	const tag = props.tag;
-	if (tag === undefined || tag === "") {
-		return (
-			<div className="query-explanation">
-				<span/>
-			</div>
-		)
-	}
-	const now = moment().format('MMMM Do YYYY, h:mm:ss a');
-	const str = `Showing <b>${logLevel}</b> log messages of <b>${tag}</b> logs from <b>${timeOption}</b> at ${now}`;
-	return (
-		<div className="query-explanation">
-			<div dangerouslySetInnerHTML={{__html: str}}/>
-		</div>
-	)
+    const logLevel = LogLevelStr(props.logLevel);
+    const {
+        startTime,
+        endTime
+    } = props;
+    const timeOption = TimeOptionString(props.timeOption, startTime, endTime);
+    const tag = props.tag;
+    if (tag === undefined || tag === "") {
+        return (
+            <div className="query-explanation">
+                <span/>
+            </div>
+        )
+    }
+    const now = moment().format('MMMM Do YYYY, HH:mm:ss');
+    const str = `Showing <b>${logLevel}</b> log messages of <b>${tag}</b> logs from <b>${timeOption}</b> at ${now}`;
+    return (
+        <div className="query-explanation">
+            <div dangerouslySetInnerHTML={{__html: str}}/>
+        </div>
+    )
 }
 
 export default QueryExplanation;
